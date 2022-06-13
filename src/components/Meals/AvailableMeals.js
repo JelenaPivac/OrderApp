9e573607c97fffbc0d6738d65d3ba3e6ae2b75aa
Jelena <jelena.pivac96@gmail.com>
2022-06-13 21:46:08 +0200

    updated links

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -10,7 +10,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://order-166b8-default-rtdb.firebaseio.com/meals.json"
+        "https://orderapp-6accf-default-rtdb.firebaseio.com/meals.json"
       );
       if (!response.ok) {
         throw new Error("Something went wrong!");
